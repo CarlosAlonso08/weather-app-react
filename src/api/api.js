b55842bc10632d47b7856be8bd9be1a4f2aef129
Carlos <111664381+CarlosAlonso08@users.noreply.github.com>
2024-09-05 02:47:04 -0600

    Update api.js
    changing the keys to not use / show the api keys

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 export const geoAPIOptions = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': '',
+		'x-rapidapi-key': process.env.REACT_APP_X_RAPIDAPI_KEY,
 		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
 	}
 
@@ -10,4 +10,4 @@ export const geoAPIOptions = {
 // Exporting the URL's of the API's and key for the use of GEO and Weather API's
 export const GEO_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const WTHR_URL = "https://api.openweathermap.org/data/2.5";
-export const WTHR_KEY = "";
+export const WTHR_KEY = process.env.REACT_APP_WTHR_KEY;;
